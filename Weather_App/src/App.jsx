5b39b11bf9736435a4988f92bacd49aafe309c06
pranjulsaxena <saxena.pranjul2005@gmail.com>
2025-03-12 21:07:02 +0530

    Weather_App

---
+++ Weather_App/src/App.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import Weather from './Components/Weather'
+import axios from 'axios';
+
+function App() {
+  const [city, setCity] = useState('');
+  const [weather, setWeather] = useState(null);
+
+  function handleCity(event) {
+    setCity(event.target.value);
+  }
+  const apiKey = "2duMLSEGoYYJduslMj+27g==i9kyT5HRPGsHx9Gy";
+  async function handleWeather() {
+    try {
+      const [citi, country] = city.split(" ");
+      const response = await axios.get(`https://api.api-ninjas.com/v1/geocoding?city=${citi}&country=${country}`, {
+        headers: {
+            'X-Api-Key': apiKey
+        }
+    });
+      
+      // API returns an array of results, take the first one
+      if (response.data && response.data.length > 0) {
+        const { latitude, longitude } = response.data[0];
+        
+        const weatherResponse = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=e8f09ab477a4021933ef738c3a98f61f`
+        );
+        
+        setWeather(weatherResponse.data);
+      } else {
+        console.error('No location data found');
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+
+  return (
+    <>
+      <input type="text" placeholder='Enter City' value={city} onChange={handleCity} />
+      <button onClick={handleWeather}>Get Weather</button>
+      {weather && <h1>{weather.name}</h1>}
+      {weather && <p>Temp is {273.15 - weather.main.temp}</p>}
+      {weather && <p>The Weather is {weather.weather[0].description}</p>}
+    </>
+  )
+}
+
+export default App
---
+++ Weather_App/src/Components/Weather.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import  "./Weather.css"
+
+function Weather() {
+  return (
+    <div>
+      Weather
+    </div>
+  )
+}
+
+export default Weather
