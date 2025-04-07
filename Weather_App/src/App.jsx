b03dd0203546e3ea46b33a89c68a7cc11b60dc89
pranjulsaxena <saxena.pranjul2005@gmail.com>
2025-04-07 22:40:45 +0530

    Weather_App

--- Weather_App/src/App.jsx
+++ Weather_App/src/App.jsx
@@ -16,10 +16,10 @@ function App() {
       const [citi, country] = city.split(" ");
       const response = await axios.get(`https://api.api-ninjas.com/v1/geocoding?city=${citi}&country=${country}`, {
         headers: {
-            'X-Api-Key': apiKey
+          'X-Api-Key': apiKey
         }
-    });
-      
+      });
+
       // API returns an array of results, take the first one
       if (response.data && response.data.length > 0) {
         const { latitude, longitude } = response.data[0];
@@ -27,7 +27,7 @@ function App() {
         const weatherResponse = await axios.get(
           `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey2}`
         );
-        
+
         setWeather(weatherResponse.data);
       } else {
         console.error('No location data found');
@@ -42,7 +42,7 @@ function App() {
       <input type="text" placeholder='Enter City' value={city} onChange={handleCity} />
       <button onClick={handleWeather}>Get Weather</button>
       {weather && <h1>{weather.name}</h1>}
-      {weather && <p>Temp is {273.15 - weather.main.temp}</p>}
+      {weather && <p>Temp is {Math.round(weather.main.temp-273.15)}</p>}
       {weather && <p>The Weather is {weather.weather[0].description}</p>}
     </>
   )
